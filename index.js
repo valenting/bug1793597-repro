82cec680a30a91b8f57f2e103c7f0872051a6894
Valentin Gosu <valentin.gosu@gmail.com>
2023-04-28 14:33:01 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const http2 = require('http2');
+const { execSync } = require('child_process');
+const fs = require('fs');
+
+// this is the domain. Change it to `example.com` or your own domain.
+const DOMAIN = 'localhost';
+
+// If the cert was already generated, use it. Otherwise generate a new one.
+if (!fs.existsSync(`${DOMAIN}.key`) || !fs.existsSync(`${DOMAIN}.cert`)) {
+  // Generate a certificate signing request (CSR)
+  const csr = execSync(`openssl req -new -newkey rsa:4096 -nodes -keyout ${DOMAIN}.key -out ${DOMAIN}.csr -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=${DOMAIN}"`).toString();
+
+  // Use the CSR to create a self-signed server certificate
+  const cert = execSync(`openssl req -new -newkey rsa:4096 -days 365 -nodes -x509 -subj "/C=US/ST=Denial/L=Springfield/O=Dis/CN=${DOMAIN}" -keyout ${DOMAIN}.key  -out ${DOMAIN}.cert`).toString();
+}
+
+// By default this creates a http2 server.
+const server = http2.createSecureServer({
+  key: fs.readFileSync(`${DOMAIN}.key`),
+  cert: fs.readFileSync(`${DOMAIN}.cert`),
+});
+
+server.on('stream', (stream, headers) => {
+  // The default path of what you want to return.
+  if (headers[':path'] === '/data') {
+
+    // Read the JSON file
+    fs.readFile('./data.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        stream.respond({ ':status': 500 });
+        stream.end('Internal Server Error');
+        return;
+      }
+
+      const contentLength = Buffer.byteLength(data, 'utf8');
+      stream.respond({
+        'content-type': 'application/json',
+        'content-length': `${contentLength}`,
+        ':status': 200
+      });
+
+      // Send the JSON data as the response
+      stream.end(data);
+    });
+  } else {
+    stream.respond({ ':status': 404 });
+    stream.end('Not Found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log(`Server listening on https://${DOMAIN}:3000`);
+});
